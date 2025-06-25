24a2d8f557d3be01975fb3feb9dcb2d0b9677f8e
Victor Retamal <victor.retamal@tii.ae>
2025-06-25 17:56:16 +0400

    Removed build files

--- workspace/build/state_sharing_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /app/workspace/src/state_sharing_demo
-# Build directory: /app/workspace/build/state_sharing_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
